1fc35cab91a22a6e5d84507230e9bd61ed248711
blvcksquad <blvckzer7@gmail.com>
2025-04-26 14:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
       coverSrc: './images/about.png',
       name: 'Naranga Paal',
       desc: 'blvckzehhh',
-      id: 1,
+      id: ,
     },
   
   ];
